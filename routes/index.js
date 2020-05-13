2f9ae826589da336c8093d08a4fa9f9d756d3ee7
Vinicius <vinicius.developer@outlook.com>
2020-05-13 12:48:16 -0300

    Criando rotas para exibir, cadastrar e marcar assitido do filme

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+// importanto o objeto de configuração do multer para usar na rota
+const upload = require("../config/upload");
+//importanto controller dos filmes
+const movieController = require("../controllers/movieController");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const router = express.Router();
+
+// Rota principal que exibira os filmes
+router.get("/", movieController.index);
+
+// rota que irá receber as informações do formulario e o poster do filme. E primeiro ela passara pro multer salvar o arquivo pra depois passar ao controller
+router.post("/cadastrar", upload.any(), movieController.store);
+// Rota para marca que ja assitiu um filme
+router.get("/assitir/:id", movieController.checkWatched);
 
 module.exports = router;
